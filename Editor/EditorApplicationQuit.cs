6abb5602ff07cca225ece473446aa1cc933a4f14
Katsumasa.Kimura <katsumasa@unity3d.com>
2022-01-19 16:10:01 +0900

    Add files via upload

---
+++ Editor/EditorApplicationQuit.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using UnityEngine.Events;
+using UnityEditor;
+
+
+namespace UTJ
+{
+    public partial class EditorApplicationExtention
+    {
+        static FieldInfo m_eitorApplicationQuitFieldInfo;
+
+        static FieldInfo eitorApplicationQuitFieldInfo
+        {
+            get
+            {
+                if (m_eitorApplicationQuitFieldInfo == null)
+                {
+                    m_eitorApplicationQuitFieldInfo = typeof(EditorApplication).GetField("editorApplicationQuit", BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic);
+                }
+                return m_eitorApplicationQuitFieldInfo;
+            }
+        }
+
+
+        /// <summary>
+        /// UnityEditor終了時に実行されるUnityAction
+        /// </summary>
+        public static UnityAction eitorApplicationQuit
+        {
+            get 
+            {
+                return eitorApplicationQuitFieldInfo.GetValue(null) as UnityAction;
+            }
+
+            set
+            {
+                var actions = eitorApplicationQuitFieldInfo.GetValue(null) as UnityAction;
+                actions += value;
+                eitorApplicationQuitFieldInfo.SetValue(null, actions);
+
+            }
+        }
+    }
+}
---
+++ Editor/Reboot.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UTJ
+{
+    public partial class EditorApplicationExtention
+    {
+        // File/Exit or File Cles prio is 240
+        [MenuItem("File/Reboot",false,241)]
+        public static void Reboot()
+        {
+            eitorApplicationQuit += RebootCB;
+            UnityEditor.EditorApplication.Exit(0);
+        }
+
+
+        //[MenuItem("Window/Test2")]
+        static void Test()
+        {
+            var argv = System.Environment.GetCommandLineArgs();
+            for (var i = 0; i < argv.Length; i++)
+            {
+                if (argv[i].Contains(" "))
+                {
+                    argv[i] = string.Format("\"{0}\"", argv[i]);
+                }
+            }
+
+            var Arguments = string.Join(" ", argv);
+        }
+
+        static void RebootCB()
+        {
+            eitorApplicationQuit -= RebootCB;
+
+            var argv = System.Environment.GetCommandLineArgs();
+            var args = new string[argv.Length - 1];
+            for(var i = 1; i < argv.Length; i++)
+            {
+                if(argv[i].Contains(" "))
+                {
+                    args[i - 1] = string.Format("\"{0}\"", argv[i]);
+                }
+                else
+                {
+                    args[i - 1] = argv[i];
+                }
+            }
+
+
+            var info = new System.Diagnostics.ProcessStartInfo();
+            info.FileName = argv[0];
+            info.Arguments = string.Join(" ", args);            
+            System.Diagnostics.Process.Start(info);
+        }
+    }
+}
